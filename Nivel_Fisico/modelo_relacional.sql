2af94d0e79c2dfdd74cf499b2d6aa0588056a940
alwinY25E <rimou111picasso@gmail.com>
2017-05-12 17:48:20 +0200

    Update modelo_relacional.sql

--- Nivel_Fisico/modelo_relacional.sql
+++ Nivel_Fisico/modelo_relacional.sql
@@ -341,45 +341,4 @@ INSERT INTO ROL_US_AS VALUES ('2', 412, 90);
 INSERT INTO ROL_US_AS VALUES ('2', 413, 90);
 INSERT INTO ROL_US_AS VALUES ('2', 421, 90);
 
-
--- Informe de Resumen de Oracle SQL Developer Data Modeler: 
--- 
--- CREATE TABLE                            21
--- CREATE INDEX                             0
--- ALTER TABLE                             47
--- CREATE VIEW                              0
--- ALTER VIEW                               0
--- CREATE PACKAGE                           0
--- CREATE PACKAGE BODY                      0
--- CREATE PROCEDURE                         0
--- CREATE FUNCTION                          0
--- CREATE TRIGGER                           0
--- ALTER TRIGGER                            0
--- CREATE COLLECTION TYPE                   0
--- CREATE STRUCTURED TYPE                   0
--- CREATE STRUCTURED TYPE BODY              0
--- CREATE CLUSTER                           0
--- CREATE CONTEXT                           0
--- CREATE DATABASE                          0
--- CREATE DIMENSION                         0
--- CREATE DIRECTORY                         0
--- CREATE DISK GROUP                        0
--- CREATE ROLE                              0
--- CREATE ROLLBACK SEGMENT                  0
--- CREATE SEQUENCE                          0
--- CREATE MATERIALIZED VIEW                 0
--- CREATE SYNONYM                           0
--- CREATE TABLESPACE                        0
--- CREATE USER                              0
--- 
--- DROP TABLESPACE                          0
--- DROP DATABASE                            0
--- 
--- REDACTION POLICY                         0
--- 
--- ORDS DROP SCHEMA                         0
--- ORDS ENABLE SCHEMA                       0
--- ORDS ENABLE OBJECT                       0
--- 
--- ERRORS                                   0
--- WARNINGS                                 0
+------------------------------------------------------- FIN ----------------------------------------------------------------------
