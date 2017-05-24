c2f577eea1a206102ac361370c4ca44552f14cdd
alwinY25E <rimou111picasso@gmail.com>
2017-05-24 03:31:26 +0200

    procedimiento apartado 3 implementado
    testing pendiente

--- PLSQL/plsql.sql
+++ PLSQL/plsql.sql
@@ -4,6 +4,7 @@
 -- 1. Crear una tabla T_PERMISOS en el usuario CAMPUS:
 
 create table T_PERMISOS (USUARIO VARCHAR2(50), OBJETO VARCHAR2(50),	FECHA	DATE, PERMISO VARCHAR2(20),	ACTIVO NUMERIC);
+
 INSERT INTO T_PERMISOS (USUARIO, OBJETO, FECHA, PERMISO, ACTIVO)
 VALUES ('USU1',	'V_PERMISOS',	'20/05/2017',	'SELECT',	1);
 
@@ -34,10 +35,26 @@ grant select on dba_tab_privs to CAMPUS;
 -- GRANT INSERT ON CAMPUS.V_RESULTADO TO USU2;
 -- REVOKE SELECT ON CAMPUS.V_PERMISOS FROM USU1;
 
-create user usu1 identified by usu;
-create user usu2 identified by usu;
---create or replace procedure 
+CREATE OR REPLACE PROCEDURE PR_DAR_PERMISOS AS
+DECLARE
+--leer los datos de la tabla T_PERMISOS de más antiguo a más reciente
+CURSOR c_permisos AS SELECT * FROM T_PERMISOS ORDER BY fecha DESC;
+CURSOR c_tab_privs;
+BEGIN
+  FOR var_permisos IN c_permisos LOOP
+  -- vaya concediendo o revocando permisos a cada uno de los usuarios de la tabla según el valor de la columna ACTIVO
+  -- ..siempre y cuando sea necesario, lo cual se comprobará en la vista DBA_TAB_PRIVS.
+  c_tab_privs IS SELECT privilege FROM DBA_TAB_PRIVS WHERE user = var_permisos.usuario AND object = var_permisos.object;
+  
+  IF var_permisos.permiso IS NOT IN c_tab_privs AND var_permisos.activo == 1 THEN
+  -- vaya concediendo o revocando permisos a cada uno de los usuarios de la tabla según el valor de la columna ACTIVO
+      EXECUTE IMMEDIATE 'GRANT '||var_permisos.permiso||' ON '||var_permisos.objeto||' TO '||var_permisos.usuario;
+  ELSE IF var_permisos.permiso IS IN c_tab_privs AND var_permisos.activo == 0 THEN
+      EXECUTE IMMEDIATE 'REVOKE '||var_permisos.permiso||' ON '||var_permisos.objeto||' FROM '||var_permisos.usuario;
+  END IF;
+  -- Si el campo Activo está a 0 para un permiso que no está otorgado tampoco se ejecuta nada. De hecho, si ejecutamos 2 veces 
+  -- seguidas el procedimiento, la segunda ejecución no cambiará los permisos, porque están como indica la tabla.
+  END LOOP;
 
+END PR_DAR_PERMISOS;
 
--- Si el campo Activo está a 0 para un permiso que no está otorgado tampoco se ejecuta nada. De hecho, si ejecutamos 2 veces 
--- seguidas el procedimiento, la segunda ejecución no cambiará los permisos, porque están como indica la tabla.
